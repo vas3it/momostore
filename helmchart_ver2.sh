29220c4cd11e05a5be3c1b1b648deaf60a50c529
Василий Сурин <surinvas@yandex.ru>
2023-04-25 12:03:32 +0000

    Add new file

---
+++ helmchart_ver2.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+CONTAINER_REGISTRY="gitlab.praktikum-services.ru:5050/momo-store-1319/momo-store"
+
+git config --global user.email "$GITLAB_USER_EMAIL"
+git config --global user.name "update-bot"
+
+mkdir -p /tmp/momo-store-iac && cd $_
+git clone git@gitlab.praktikum-services.ru:momo-store-1319/momo-store-iac.git .
+git switch $CI_COMMIT_REF_NAME
+
+# Update helm-chart version
+sed -i -e "s/^appVersion.*/appVersion: $VERSION/" -e "s/^version.*/version: $VERSION/" ./momo-store-chart/Chart.yaml
+
+# Update backend version if docker image was build
+if docker manifest inspect "$CONTAINER_REGISTRY/backend:$VERSION" > /dev/null ; then
+    sed -i -e "/^  - name: backend$/{n;s/^    version.*/    version: $VERSION/;}" ./momo-store-chart/Chart.yaml
+    sed -i -e "s/^appVersion.*/appVersion: $VERSION/" -e "s/^version.*/version: $VERSION/" ./momo-store-chart/charts/backend/Chart.yaml
+    echo "Backend version has been updated."
+else
+    echo "Backend docker image didn't build in this pipeline.\nBackend version has not been updated."
+fi
+
+# Update frontend version if docker image was build
+if docker manifest inspect "$CONTAINER_REGISTRY/frontend:$VERSION" > /dev/null ; then
+    sed -i -e "/^  - name: frontend$/{n;s/^    version.*/    version: $VERSION/;}" ./momo-store-chart/Chart.yaml
+    sed -i -e "s/^appVersion.*/appVersion: $VERSION/" -e "s/^version.*/version: $VERSION/" ./momo-store-chart/charts/frontend/Chart.yaml
+    echo "Frontend version has been updated."
+else
+    echo "Frontend docker image didn't build in this pipeline.\nFrontend version has not been updated."
+fi
+
+git commit -a -m "Updated to version $VERSION"
+git push origin $CI_COMMIT_REF_NAME
+
+exit 0
