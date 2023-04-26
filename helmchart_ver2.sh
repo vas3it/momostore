0b8f48b3ef0c310066d48a81983a199b8f286c43
Василий Сурин <surinvas@yandex.ru>
2023-04-26 08:26:12 +0000

    Update helmchart_ver2.sh

--- helmchart_ver2.sh
+++ helmchart_ver2.sh
@@ -7,7 +7,8 @@ git config --global user.name "update-bot"
 
 mkdir -p /tmp/momo-store-iac && cd $_
 #git clone https://$CI_REGISTRY_USER:$CI_REGISTRY_PASSWORD@gitlab.praktikum-services.ru/v.surin/momostore.git .
-git clone v.surin@gitlab.praktikum-services.ru:momostore/momo-store-iac.git .
+#git clone v.surin@gitlab.praktikum-services.ru:momostore/momo-store-iac.git .
+git clone https://gitlab-ci-token:${CI_JOB_TOKEN}gitlab.praktikum-services.ru:v.surin/momostore.git .
 #git clone ${SPECGIT} .
 git switch $CI_COMMIT_REF_NAME
 
